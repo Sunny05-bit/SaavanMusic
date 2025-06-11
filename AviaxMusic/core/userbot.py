611e35fbfc01dc2cb7917fb09340b28d29da5f71
Sunny05-bit <sunnykumar9721665358@gmail.com>
2025-06-11 22:54:41 +0530

    Update userbot.py

--- AviaxMusic/core/userbot.py
+++ AviaxMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("AviaxOfficial")
-                await self.one.join_chat("AviaxSupport")
+                await self.one.join_chat("musicbotupdate")
+                await self.one.join_chat("musicbotupdate")
             except:
                 pass
             assistants.append(1)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("AviaxOfficial")
-                await self.one.join_chat("AviaxSupport")
+                await self.five.join_chat("musicbotupdate")
+                await self.one.join_chat("musicbotupdate")
             except:
                 pass
             assistants.append(5)
